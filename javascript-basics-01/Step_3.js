dec15ab79ad902851ab80305e1761b63233cf9a6
Ali-Traboulsi <alitraboulsi112.gmail.com>
2020-09-28 11:49:25 +0300

    Completed-step-

--- javascript-basics-01/Step_3.js
+++ javascript-basics-01/Step_3.js
@@ -1,8 +1,2 @@
 var name = window.prompt("Enter your name:","");
 window.alert("Hello " + name);
-
-function alertWithoutNotice(message){
-    setTimeout(function(){
-        alert(message);
-    }, 1000);
-}
---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+var name, surname, city;
+name = window.prompt("Enter your name: ");
+surname = window.prompt("Enter your surname: ");
+city = window.prompt("Here's a quick question " + name + " " + surname + "\nWhere do you live?: ");
+
+window.alert("Welcome Abroad " + name + " " + surname + "!" + "\nGlad that you are living in " + city + ".");
