fe9035564028bab67a8a0581a1545c76bd8075cc
Ali-Traboulsi <alitraboulsi112.gmail.com>
2020-09-28 12:13:45 +0300

    Completed-step-6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,14 @@
+var num1, num2;
+
+//info window
+window.alert("We will find the remainder of the division of two numbers! Go!")
+
+//user input
+num1 = window.prompt("Enter a number: ");
+num2 = window.prompt("Then enter another number: ");
+
+var answer = num1 % num2;
+
+window.alert("Here you go! The remainder of the division of the two numbers " 
++ "\n" + num1 + " and " + num2 + " is: " + answer);
+
