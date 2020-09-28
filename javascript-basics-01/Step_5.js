c1a661c6b763362c98f7cd4fc04a58285229c96b
Ali-Traboulsi <alitraboulsi112.gmail.com>
2020-09-28 11:58:10 +0300

    Completed-step-5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,10 @@
+var input1, input2;
+
+// ask the user the enter them
+window.alert("We will go ahead with some multiplication.. GO")
+input1 = window.prompt("Please enter a number: ")
+input2 = window.prompt("Please enter another number: ")
+
+var answer = input1 * input2;
+//display as a window
+window.alert("The product of " + input1 + " and " + input2 + " is: " + answer);
